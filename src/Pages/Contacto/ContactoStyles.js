e60f018c502b2c0c7c598d1e3328cdb5c3f976b1
Yanina S. Pernice <ypernice@gmail.com>
2023-08-21 23:12:33 -0300

    Cambios de rutas de imagenes 5

--- src/Pages/Contacto/ContactoStyles.js
+++ src/Pages/Contacto/ContactoStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import pattern_sprinkles from '../Images/pattern_sprinkles.png'
 
 export const ContactoContainer = styled.div`
     width: 100%;
@@ -7,7 +8,7 @@ export const ContactoContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-image: url('../public/images/Backgrounds/pattern_sprinkles.png');
+    background-image: url(${pattern_sprinkles});
     background-repeat: repeat;
     background-size: contain;
     background-position: center;
